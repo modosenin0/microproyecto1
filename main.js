556242c2c2b514d980337bffb204be15710ea19e
modosenin0 <97416036+modosenin0@users.noreply.github.com>
2023-02-05 11:41:01 -0400

    Contact1.0

--- main.js
+++ main.js
@@ -1,44 +1,112 @@
+const img = document.querySelectorAll(".slide");
+const nxtimg = document.querySelector(".btn-next");
+let presentimg = 0;
+const pastimg = document.querySelector(".btn-prev");
+let maximg = img.length - 1;
 
-const slides = document.querySelectorAll(".slide");
-
-slides.forEach((slide, indx) => {
+img.forEach((slide, indx) => {
   slide.style.transform = `translateX(${indx * 100}%)`;
 });
 
-let curSlide = 0;
-const nextSlide = document.querySelector(".btn-next");
-nextSlide.addEventListener("click", function () {
-     curSlide++;
-
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+nxtimg.addEventListener("click", function () {
+  if (presentimg === maximg) {
+    presentimg = 0;
+  } else {
+    presentimg++;
+  }
+  img.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - presentimg)}%)`;
   });
 });
 
-let maxSlide = slides.length - 1;
 
-nextSlide.addEventListener("click", function () {
-  if (curSlide === maxSlide) {
-    curSlide = 0;
+pastimg.addEventListener("click", function () {
+  if (presentimg === 0) {
+    presentimg = maximg;
   } else {
-    curSlide++;
+    presentimg--;
   }
 
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  img.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - presentimg)}%)`;
   });
 });
 
-const prevSlide = document.querySelector(".btn-prev");
+const contact = document.querySelector(".contact-boton");
+const nombre = document.getElementById("nombre");
+const email = document.getElementById ("emailg")
+const message = document.getElementById("message")
 
-prevSlide.addEventListener("click", function () {
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
+
+function validateName(name){
+  var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+  var name = document.getElementById('nombre').value;
+  if(!regName.test(name)){
+      alert('Invalid name given.');
+      return false
+  }else{
+      alert('Valid name given.');
+      return true
   }
+}
 
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
+function validateEmail(email) {
+  const validEmail = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
+  if (email.value.match(validEmail)) {
+    return true;
+  } else {
+    alert("No valid email")
+    return false;
+  };
+};
+
+function validateMessage(input){
+  if(input.value === ""){
+    alert("No escribiste ningun mensaje")
+    return false
+  }
+  else{
+    return true
+  }
+}
+
+contact.addEventListener("click", function(){
+  if(validateName(nombre.value) === true && validateEmail(email) === true && validateMessage(message) === true){
+    alert("Información valida")
+  }else{
+    alert("Información no valida")
+  }
+})
+
+const mat = document.querySelector(".mat-bars");
+let stringResult = "";
+
+const skills = {
+    "HTML": 40,
+    "CSS":20,
+    "JS": 60,
+    "Python": 65,
+    "Java": 30
+};
+
+function writeHTML() {
+  for (const key in skills) {
+      const stringStructure = ` 
+      <style>
+      #${key} {
+      width: ${skills[key]}%;
+      }
+      </style>
+      <div class='bar-and-name'>
+          <div class='bar-and-name'>
+              <h1 class='bar-name'>${key}</h1>
+                  <div class='graph-space'>
+                  <div id='${key}' class='graph'>${skills[key]}%</div>
+          </div>
+      </div>`;
+      stringResult += stringStructure;
+  };
+  mat.innerHTML = stringResult;
+};
+
+writeHTML();
